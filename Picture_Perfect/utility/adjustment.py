1d782565baf4048c73614681ec7dfe6053c68f27
Shubham Agarwal <49315586+Shubhamag12@users.noreply.github.com>
2021-05-29 19:22:28 +0530

    Adding threading to the adjustment file

--- Picture_Perfect/utility/adjustment.py
+++ Picture_Perfect/utility/adjustment.py
@@ -22,55 +22,54 @@ def cam_adjustment(path):
     rot = [0, 0, 0]
     scale = [1, 1, 1]
     shear = [0, 0, 0]
-    
-    temp = Projection(transform(img_input, trans, rot, scale, shear), 'image_input') # 0
-    img_arr.append(temp)
-    
-    temp = Projection(transform(img_input, [45,0,15], [0,10,0], scale, shear), 'image_vertical_ax_rot+') # 1
-    img_arr.append(temp)
-    
-    temp = Projection(transform(img_input, [45,0,15], [0,10,0], scale, shear), 'image_vertical_ax_rot-') # 2
-    img_arr.append(temp)
-    
-    temp = Projection(transform(img_input, [0,45,15], [10,0,0], scale, shear), 'image_horiz_ax_rot+') # 3
-    img_arr.append(temp)
-    
-    temp = Projection(transform(img_input, [0,45,15], [10,0,0], scale, shear), 'image_horiz_ax_rot-') # 4
-    img_arr.append(temp)
-    
-    temp = Projection(transform(img_input, trans, [0,0,15], scale, shear), 'image_rotate+') # 5
-    img_arr.append(temp)
-    
-    temp = Projection(transform(img_input, trans, [0,0,-15], scale, shear), 'image_rotate-') # 6
-    img_arr.append(temp)
-    
-    temp = Projection(transform(img_input, [0,0,25], rot, scale, shear), 'image_zoom+') # 7
-    img_arr.append(temp)
-    
-    temp = Projection(transform(img_input, [0,0,-25], rot, scale, shear), 'image_zoom-') # 8
-    img_arr.append(temp)
-    
-    img_arr2 = img_arr.copy()
+
     bright_mask = np.full(img_input.shape, 30)
     
+    def func1(list1, list2, mode, iter):
+        temp = Projection(transform(img_input, list1, list2, scale, shear), mode) # 1
+        img_arr.append(temp)
+        temp = Projection(img_arr[iter].img + bright_mask, img_arr[iter].title+'_bright+')
+        img_arr.append(temp)
+        temp = Projection(img_arr[iter].img - bright_mask, img_arr[iter].title+'_bright-')
+        img_arr.append(temp)
+        print(type(img_input))
+        print(len(img_arr))
+
     # toolbar for progress tracking
     toolbar_width = len(img_arr)
     sys.stdout.write("[%s]" % (" " * toolbar_width))
     sys.stdout.flush()
     sys.stdout.write("\b" * (toolbar_width+1)) # return to start of line, after '['
-    for i in img_arr:
-        temp = Projection(i.img + bright_mask, i.title+'_bright+')
-        img_arr2.append(temp)
-        temp = Projection(i.img - bright_mask, i.title+'_bright-')
-        img_arr2.append(temp)
-        # update the bar
-        sys.stdout.write("-")
-        sys.stdout.flush()
+
+    tr1 = threading.Thread(target=func1, args=([45,0,15], [0,10,0], 'image_vertical_ax_rot+', 0))
+    tr2 = threading.Thread(target=func1, args=([45,0,15], [0,10,0], 'image_vertical_ax_rot-', 1))
+    tr3 = threading.Thread(target=func1, args=([0,45,15], [10,0,0], 'image_horiz_ax_rot+', 2))
+    tr4 = threading.Thread(target=func1, args=([0,45,15], [10,0,0], 'image_vertical_ax_rot-', 3))
+    tr5 = threading.Thread(target=func1, args=(trans, [0,0,15], 'image_rotate+', 4))
+    tr6 = threading.Thread(target=func1, args=(trans, [0,0,15], 'image_rotate-', 5))
+    tr7 = threading.Thread(target=func1, args=([0,0,-25], rot, 'image_zoom+', 6))
+    tr8 = threading.Thread(target=func1, args=([0,0,-25], rot, 'image_zoom-', 7))
+
+    tr1.start()
+    tr2.start()
+    tr3.start()
+    tr4.start()
+    tr5.start()
+    tr6.start()
+    tr7.start()
+    tr8.start()
+
+    tr1.join()
+    tr2.join()
+    tr3.join()
+    tr4.join()
+    tr5.join()
+    tr6.join()
+    tr7.join()
+    tr8.join()
 
     sys.stdout.write("]\n") # this ends the progress bar
 
-    img_arr = img_arr2
-    
     # fig.add_subplot(rows, columns, count)
     img_arr.sort(key=lambda x: x.score, reverse=True)
 
