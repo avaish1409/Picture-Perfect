dcf51d7ca252cea154594f7ea62c66a53b096ff0
Shubham Agarwal <49315586+Shubhamag12@users.noreply.github.com>
2021-05-29 19:23:53 +0530

    Updated home function (#5)
    * Updated home function
    
    * Updated request

--- Picture_Perfect/views.py
+++ Picture_Perfect/views.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 from utility import adjustment
 
-
-res = adjustment.cam_adjustment('/home/anirudh/Desktop/aov.png')
-print('='*70)
-print('To API: ', res)
+def home(request):
+  img = request.POST['image_input']
+  res = adjustment.cam_adjustment(img)
+  print('='*70)
+  print('To API: ', res)
+  method = res[0]
+  score = res[1]
+  text = f'Method: {method}, Score: {Score}'
+  return HttpResponse(text)
